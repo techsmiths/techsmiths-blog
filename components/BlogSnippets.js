bcacb08dc78449c9423d19d8190cff00c090cf18
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-07-12 01:29:26 +0600

    added a bit to the pagination logic
    Not yet tested or perfected

--- components/BlogSnippets.js
+++ components/BlogSnippets.js
@@ -6,27 +6,76 @@ import Link from "next/link"
 
 const BlogSnippets = ({ blogs }) => {
 
-    const [blogList, setBlogList] = useState(blogs);
 
     // set up pagination
     const [currentPage, setCurrentPage] = useState(1);
-    const blogsPerPage = 3;
+    const blogsPerPage = 1;
 
     // get current blogs
     const indexOfLastBlog = currentPage * blogsPerPage;
     const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
-    const currentBlogs = blogList.slice(indexOfFirstBlog, indexOfLastBlog);
+    const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
 
     // change page (make sure the page number is within the range of the number of blogs)
-    const paginate = (pageNumber) =>  {
+    const paginate = (pageNumber) => {
         if (pageNumber < 1) {
             setCurrentPage(1);
-        } else if (pageNumber > Math.ceil(blogList.length / blogsPerPage)) {
-            setCurrentPage(Math.ceil(blogList.length / blogsPerPage));
+        } else if (pageNumber > Math.ceil(blogs.length / blogsPerPage)) {
+            setCurrentPage(Math.ceil(blogs.length / blogsPerPage));
         } else {
             setCurrentPage(pageNumber);
         }
     }
+
+    const renderNumberedButtons = () => {
+
+        // calculate the number of pages
+        const totalPages = Math.ceil(blogs.length / blogsPerPage);
+
+        // a range of buttons to display
+        let startPage;
+        let endPage;
+
+        // if there are less than 5 pages, display all of them
+        if (totalPages <= 5) {
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+
+            // if the current page is less than 3, display the first 5 pages
+            if (currentPage <= 3) {
+                startPage = 1;
+                endPage = 5;
+            }
+            // if the current page is greater than the last 3 pages, display the last 5 pages
+            else if (currentPage >= totalPages - 2) {
+                startPage = totalPages - 4;
+                endPage = totalPages;
+            } else 
+            // otherwise, display the 2 pages before and after the current page
+            {
+                startPage = currentPage - 2;
+                endPage = currentPage + 2;
+            }
+        }
+
+        const buttons = [];
+        for (let i = startPage; i <= endPage; i++) {
+            buttons.push(
+                <button
+                    key={i}
+                    className={`${styles.paginationButton} ${i === currentPage ? styles.activeButton : ''}`}
+                    onClick={() => paginate(i)}
+                >
+                    {i}
+                </button>
+            );
+        }
+
+        return buttons;
+    };
+
+
 
 
     return (
@@ -48,11 +97,12 @@ const BlogSnippets = ({ blogs }) => {
                 ))}
             </div>
             <div className={styles.paginationBar}>
-                <p className={styles.paginationText}>Page {currentPage} of {Math.ceil(blogList.length / blogsPerPage)}</p>
+                <p className={styles.paginationText}>Page {currentPage} of {Math.ceil(blogs.length / blogsPerPage)}</p>
                 <div className={styles.paginationButtons}>
                     <button className={styles.paginationButton} onClick={() => paginate(currentPage - 1)}>
                         Previous
                     </button>
+                    {renderNumberedButtons()}
                     <button className={styles.paginationButton} onClick={() => paginate(currentPage + 1)}>
                         Next
                     </button>
