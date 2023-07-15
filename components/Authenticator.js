5850573bdc78099b1399764222e87ff6774490d3
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-07-15 06:01:45 +0600

    designed authenticator, added logo

--- components/Authenticator.js
+++ components/Authenticator.js
@@ -6,19 +6,31 @@ import useFirebase from "../hooks/useFirebase"
 function Authenticator() {
     const { user, isLoading, signInWithGoogle, error, signOut } = useFirebase();
 
+
+    function getScreenName() {
+        if (!user) return "";
+
+        if (user.displayName && user.displayName !== "") {
+            return user.displayName
+        }
+
+        return user.email
+    }
+
     return (
         <div className={styles.auth}>
             {user ? (
                 <div className={styles.user}>
                     <img src={user.photoURL} alt={user.displayName} className={styles.userImage} />
-                    <p className={styles.userName}>{user.displayName}</p>
-                    <button className={styles.authButton} onClick={signOut}>
-                        Sign Out
+                    <p className={styles.userName}>{getScreenName()}</p>
+                    <button className={styles.logoutButton} onClick={signOut}>
+                        <img src="/logout.png" alt="logout Logo" className={styles.googleLogo} />
                     </button>
                 </div>
             ) : (
                 <button className={styles.authButton} onClick={signInWithGoogle}>
-                    Sign In
+                    Google
+                    <img src="/google.png" alt="Google Logo" className={styles.googleLogo} />
                 </button>
             )}
         </div>
--- components/BlogSnippets.js
+++ components/BlogSnippets.js
@@ -6,9 +6,9 @@ import Link from "next/link"
 import Authenticator from "../components/Authenticator.js"
 
 const BlogSnippets = ({ blogs }) => {
-    
 
-    
+
+
 
 
     // set up pagination
@@ -145,8 +145,10 @@ const BlogSnippets = ({ blogs }) => {
     return (
         <div className={styles.homepage}>
             <div className={styles.header}>
-                <img src="/favicon.ico" alt="Techsmiths logo" className={styles.logo} />
-                <h1 className={styles.heading}>Techsmiths@blog</h1>
+                <div className={styles.logoContainer}>
+                    <img src="/logo.png" alt="Techsmiths logo" className={styles.logo} />
+                    <h1 className={styles.heading}>Techsmiths@blog</h1>
+                </div>
                 <Authenticator />
             </div>
             <div className={styles.blogGrid}>
